566ee951a376498070a2f65a1637d873a1c3eb03
Gediminas Kaminskas <kamgediminas@gmail.com>
2021-10-24 13:40:59 +0300

    Icons in BarTab

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Button, View } from 'react-native';
 import HomeScreen from '../screens/Home';
 import SettingsScreen from '../screens/Settings';
 import { useSelector } from 'react-redux';
 import c from '../constants/colors';
 import StatusBar from '../components/UI/StatusBar';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -41,13 +41,33 @@ const DarkTheme = {
 const Navigation = () => {
   const theme = useSelector(({ settings }: any) => settings.theme);
 
+  const currentTheme = theme === 'dark' ? DarkTheme : LightTheme;
+
   return (
-    <NavigationContainer theme={theme === 'dark' ? DarkTheme : LightTheme}>
+    <NavigationContainer theme={currentTheme}>
       <StatusBar />
       <Tab.Navigator
-        screenOptions={{
+        screenOptions={({ route }) => ({
+          // Hides the top header
           header: () => false,
-        }}
+          // Hides screen labels in navigation
+          tabBarShowLabel: false,
+          // Custom icons for each route
+          tabBarIcon: () => {
+            if (route.name === 'Home') {
+              return (
+                <MaterialCommunityIcons name="home" size={42} color={currentTheme.colors.primary} />
+              );
+            }
+            if (route.name === 'Settings') {
+              return (
+                <MaterialCommunityIcons name="cog" size={42} color={currentTheme.colors.primary} />
+              );
+            }
+          },
+          // Whole bar style to remove top border
+          tabBarStyle: { borderTopWidth: 0, elevation: 0, height: 80 },
+        })}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
